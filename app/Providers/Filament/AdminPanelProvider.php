bdaca83fd3c28eee099985b141ea1bd1302d6d97
Thiago Fernando da Rosa <thyagogoth@gmail.com>
2023-10-14 19:15:36 -0300

    add themes switcher funcionalities

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -16,6 +16,8 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Hasnayeen\Themes\Http\Middleware\SetTheme;
+use Hasnayeen\Themes\ThemesPlugin;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -68,6 +70,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+                SetTheme::class
             ])
             ->plugins([
                 ActivitylogPlugin::make()
@@ -91,6 +94,9 @@ class AdminPanelProvider extends PanelProvider
                 StickyHeaderPlugin::make(),
 
                 SpotlightPlugin::make(),
+
+                ThemesPlugin::make()
+//                    ->canViewThemesPage(fn () => auth()->user()?->is_admin)
 
 //                FilamentTourPlugin::make()
             ])
